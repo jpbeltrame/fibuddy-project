ecb97a9de1887884a57cc1ecce20f70bd761eb05
Joao Pedro Beltrame <jpbeltrame@gmail.com>
2023-11-13 02:21:34 -0800

    Creating fragments for: - navbar - header import - footer

--- src/main/java/com/csis3275/fitbuddyproject/Web/SiteController.java
+++ src/main/java/com/csis3275/fitbuddyproject/Web/SiteController.java
@@ -11,21 +11,25 @@ public class SiteController {
 
     @GetMapping(path = "/")
     public String IndexPageLoader(Model model) {
+        model.addAttribute("page", "main");
         return "main";
     }
 
     @GetMapping(path = "/about")
     public String AboutPageLoader(Model model) {
+        model.addAttribute("page", "about");
         return "about";
     }
 
     @GetMapping(path = "/support")
     public String SupportPageLoader(Model model) {
+        model.addAttribute("page", "support");
         return "support";
     }
 
     @GetMapping(path = "/eula")
     public String EULAPageLoader(Model model) {
+        model.addAttribute("page", "eula");
         return "eula";
     }
 }
