988420fe90942a8f969ba093592ef6a22239403c
Moises <moisescls16@gmail.com>
2023-11-12 17:56:44 -0800

    Criacao das classes, algumas estao com extends mas outras nao, verificar no futuro, metodos criados tambem.

---
+++ src/main/java/com/csis3275/fitbuddyproject/Entities/Admin.java
@@ -0,0 +1,13 @@
+package com.csis3275.fitbuddyproject.Entities;
+
+public class Admin {
+    String role;
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
---
+++ src/main/java/com/csis3275/fitbuddyproject/Entities/PersonalTrainer.java
@@ -0,0 +1,13 @@
+package com.csis3275.fitbuddyproject.Entities;
+
+public class PersonalTrainer {
+    String specialty;
+
+    public String getSpecialty() {
+        return specialty;
+    }
+
+    public void setSpecialty(String specialty) {
+        this.specialty = specialty;
+    }
+}
