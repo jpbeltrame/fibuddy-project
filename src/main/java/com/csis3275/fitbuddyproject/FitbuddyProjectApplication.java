7edf5c838578d123a5f2adced75a478438686f45
Joao Pedro Beltrame <jpbeltrame@gmail.com>
2023-11-12 23:28:58 -0800

    init

---
+++ src/main/java/com/csis3275/fitbuddyproject/FitbuddyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.csis3275.fitbuddyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitbuddyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitbuddyProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/csis3275/fitbuddyproject/FitbuddyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csis3275.fitbuddyproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitbuddyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
